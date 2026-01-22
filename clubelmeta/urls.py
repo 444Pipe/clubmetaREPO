b73b96c5dd4d0db18fd438dc25a2b30482933670
444Pipe <delgadofelipe315@gmail.com>
2026-01-22 16:33:19 -0500

    Add menu URLs and templates for Comunicados, Junta, Deportes, Actividades, Gastronomía, Administración and subpages

--- clubelmeta/urls.py
+++ clubelmeta/urls.py
@@ -30,6 +30,24 @@ urlpatterns = [
     path('bienvenidos/vision/', TemplateView.as_view(template_name='vision.html'), name='vision'),
     path('bienvenidos/mision/', TemplateView.as_view(template_name='mision.html'), name='mision'),
     path('bienvenidos/instalaciones/', TemplateView.as_view(template_name='instalaciones.html'), name='instalaciones'),
+    # Comunicados y administración
+    path('comunicados/', TemplateView.as_view(template_name='comunicados.html'), name='comunicados'),
+    path('junta-directiva/', TemplateView.as_view(template_name='junta_directiva.html'), name='junta_directiva'),
+    path('actividades/', TemplateView.as_view(template_name='actividades.html'), name='actividades'),
+    path('administracion/', TemplateView.as_view(template_name='administracion.html'), name='administracion'),
+    # Deportes y subpáginas
+    path('deportes/', TemplateView.as_view(template_name='deportes.html'), name='deportes'),
+    path('deportes/tenis/', TemplateView.as_view(template_name='deportes/tenis.html'), name='deportes_tenis'),
+    path('deportes/natacion/', TemplateView.as_view(template_name='deportes/natacion.html'), name='deportes_natacion'),
+    path('deportes/futbol/', TemplateView.as_view(template_name='deportes/futbol.html'), name='deportes_futbol'),
+    path('deportes/gimnasio/', TemplateView.as_view(template_name='deportes/gimnasio.html'), name='deportes_gimnasio'),
+    # Gastronomía and subpages
+    path('gastronomia/', TemplateView.as_view(template_name='gastronomia.html'), name='gastronomia'),
+    path('gastronomia/salones-eventos/', TemplateView.as_view(template_name='gastronomia/salones_eventos.html'), name='gastronomia_salones'),
+    path('gastronomia/refrigerios-empresariales/', TemplateView.as_view(template_name='gastronomia/refrigerios_empresariales.html'), name='gastronomia_refrigerios'),
+    path('gastronomia/eventos-sociales/', TemplateView.as_view(template_name='gastronomia/eventos_sociales.html'), name='gastronomia_eventos'),
+    path('gastronomia/infantil-juvenil/', TemplateView.as_view(template_name='gastronomia/infantil_juvenil.html'), name='gastronomia_infantil'),
+    path('gastronomia/pasabocas-cocktail/', TemplateView.as_view(template_name='gastronomia/pasabocas_cocktail.html'), name='gastronomia_pasabocas'),
 ]
 
 # Servir archivos estáticos en desarrollo
