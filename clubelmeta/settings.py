97894c7972af052a149585078ec2642f2a1b08d4
444Pipe <delgadofelipe315@gmail.com>
2026-04-20 22:26:22 -0500

    fix: improve robustness of EMAIL_USE_TLS parsing and support EMAIL_USE_SSL

--- clubelmeta/settings.py
+++ clubelmeta/settings.py
@@ -63,7 +63,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 # Legacy SMTP settings
 EMAIL_HOST = os.getenv("EMAIL_HOST", "")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True").lower() in ("true", "1", "t", "yes")
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False").lower() in ("true", "1", "t", "yes")
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
